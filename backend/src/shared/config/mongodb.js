f8ddf406f9c57afa98aa939146ca9edf4d5692b7
uzumaki902 <saimohanty189@gmail.com>
2026-03-13 08:36:09 +0530

    dfgfdgdf

--- backend/src/shared/config/mongodb.js
+++ backend/src/shared/config/mongodb.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import config from "./index";
 import logger from "./logger";
-//nice lesggooo hehehe g fgefgfd
+//nice lesggooo hehehe g  egergregergergerg
 const connectDB = async () => {
   try {
     await mongoose.connect(config.database_url);
