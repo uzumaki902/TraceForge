f6662c70a607ed2e27fa2add620a1abcba19050c
uzumaki902 <saimohanty189@gmail.com>
2026-04-22 23:43:04 +0530

    made some changes

--- backend/src/shared/config/index.js
+++ backend/src/shared/config/index.js
@@ -3,29 +3,30 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-  // Server configuration
+  // Server
   node_env: process.env.NODE_ENV || "development",
   port: parseInt(process.env.PORT || "5000", 10),
 
-  //mongodb configurations
-
-
-  mongo:{
-     mongo_uri: process.env.MONGO_URI || "mongodb://localhost:27017/api_monitoring",
-    dbName:process.env.DB_NAME || "api_monitoring",
+  // MongoDB
+  mongo: {
+    uri: process.env.MONGO_URI || "mongodb://localhost:27017/api_monitoring",
+    dbName: process.env.MONGO_DB_NAME || "api_monitoring",
   },
 
+  // PostgreSQL
   postgres: {
     host: process.env.PG_HOST || "localhost",
     port: parseInt(process.env.PG_PORT || "5432", 10),
     user: process.env.PG_USER || "postgres",
-    password: process.env.PG_PASSWORD || "postgres",
+    password: process.env.PG_PASSWORD || "sai",
     database: process.env.PG_DATABASE || "api_monitoring",
-
   },
+
+  // RabbitMQ
   rabbitmq: {
-    url: process.env.RABBITMQ_URL || "amqp://localhost",
-    queue: process.env.RABBITMQ_QUEUE || "api_monitoring_queue",
-  }, 
-   
-}
+    url:
+      process.env.RABBITMQ_URL ||
+      "amqp://api_user:secure_password@localhost:5672/api_monitoring",
+    queue: process.env.RABBITMQ_QUEUE || "api_hits",
+  },
+};
